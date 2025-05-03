8ef35ccd5060fd4502bf241472dd07a1c491d537
friday ogboji <friday.ogboji100@gmail.com>
2025-05-03 12:33:19 +0100

    Added more product

--- src/data/products.ts
+++ src/data/products.ts
@@ -1,11 +1,18 @@
 // src/data/products.ts
 
 export const products = [
-    { name: 'Tomatoes', image: '/fresh-tomatos.jpeg', price: 2000 },
+    { name: 'Tomatoes', image: '/fresh-tomatos.jpeg', price: 10000 },
     { name: 'Yam', image: '/yam.jpg', price: 3000 },
-    { name: 'Plantain', image: '/plantain.webp', price: 4000 },
-    { name: 'Tomatoes', image: '/cow.png', price: 2000 },
-    { name: 'Yam', image: '/dry-fish.jpeg', price: 3000 },
-    { name: 'Plantain', image: '/fresh-fish.png', price: 4000 },
+    { name: 'Plantain', image: '/plantain.webp', price: 2000 },
+    { name: 'Well Fed Cow', image: '/cow.png', price: 300000 },
+    { name: 'High Quaility Dry Fish', image: '/dry-fish.jpeg', price: 5500 },
+    { name: 'Fresh Fish', image: '/fresh-fish.png', price: 5000 },
+
+    { name: 'Newly Baked Garri', image: '/garri.png', price: 9000 },
+    { name: 'YUM YUM Fufu Cassava', image: '/fufu-cassava.png', price: 10500 },
+    { name: 'Nigeria Fresh Veg&Fruits', image: '/f$v.png', price: 2000 },
+    { name: 'Fresh Naija Cassava', image: '/cassava1.png', price: 3500 },
+    { name: 'Fresh Naija Cassava', image: 'cassava.png', price: 5500 },
+    { name: 'Newly farm produce Beans', image: '/beans.png', price: 7000 },
   ];
   
